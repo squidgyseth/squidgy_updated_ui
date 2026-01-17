7736f8df805cef627aa7b95df99c3c048b5b2a0e
Melda Igde <igdemelda@gmail.com>
2026-01-17 12:52:20 +0300

    fix: industry field and typescript errors

--- client/pages/agents/smm_assistant/index.tsx
+++ client/pages/agents/smm_assistant/index.tsx
@@ -6,11 +6,10 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Import all pages
-import SmmAssistantSmmAssistantAbc123SMMDashboardPage1 from './smm_assistant_abc123_smm_dashboard_page1';
-import SmmAssistantSmmAssistantDef456AnalyticsViewPage2 from './smm_assistant_def456_analytics_view_page2';
-import SmmAssistantSmmAssistantGhi789ContentCreatorPage3 from './smm_assistant_ghi789_content_creator_page3';
-import SmmAssistantSmmAssistantLiquidBlanch17032840Page4 from './smm_assistant_liquid_blanch_17032840_page4';
-import SmmAssistantSmmAssistantAnalyticsDemo12345Page5 from './smm_assistant_analytics_demo_12345_page5';
+import SmmAssistantSmmAssistantAbc123SMMDashboardPage1 from './smm_assistant_abc123_smm_dashboard_page1.tsx';
+import SmmAssistantSmmAssistantDef456AnalyticsViewPage2 from './smm_assistant_def456_analytics_view_page2.tsx';
+import SmmAssistantSmmAssistantGhi789ContentCreatorPage3 from './smm_assistant_ghi789_content_creator_page3.tsx';
+import SmmAssistantSmmAssistantLiquidBlanch17032840Page4 from './smm_assistant_liquid_blanch_17032840_page4.tsx';
 
 export default function SmmAssistantAgent() {
   return (
@@ -20,7 +19,6 @@ export default function SmmAssistantAgent() {
       <Route path="smm-assistant-def456-analytics-view-page2" element={<SmmAssistantSmmAssistantDef456AnalyticsViewPage2 />} />
       <Route path="smm-assistant-ghi789-content-creator-page3" element={<SmmAssistantSmmAssistantGhi789ContentCreatorPage3 />} />
       <Route path="smm-assistant-liquid-blanch-17032840-page4" element={<SmmAssistantSmmAssistantLiquidBlanch17032840Page4 />} />
-      <Route path="smm-assistant-analytics-demo-12345-page5" element={<SmmAssistantSmmAssistantAnalyticsDemo12345Page5 />} />
     </Routes>
   );
 }
@@ -46,9 +44,6 @@ export const agentConfig = {
       "name": "smm_assistant_liquid-blanch-17032840_page4",
       "path": "smm-assistant-liquid-blanch-17032840-page4"
     },
-    {
-      "name": "smm_assistant_analytics-demo-12345_page5",
-      "path": "smm-assistant-analytics-demo-12345-page5"
-    }
+
   ]
 };
